d237d14034e4687c96f3bd6f8aa9a76de28f2e69
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-05-15 12:45:48 -0400

    database file

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.USERNAME,
+    host:"localhost",
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: 5432
+});
+
+module.exports = pool;
